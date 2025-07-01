6882411a94ae149fd686ac20abec66d71237dbab
Kodlak15 <stanlcod15@protonmail.com>
2025-07-01 15:48:38 -0700

    Initial commit

--- flake.nix
+++ flake.nix
@@ -2,12 +2,9 @@
   description = "My collection of flake templates";
 
   outputs = {self}: {
-builtins.map (fn: {fn.path = ./fn;}) (pkgs.lib.fileset.toList ./.)
-    templates = builtins.listToAttrs ;
+    templates = {
+      example.path = ./example;
+      phoenix.path = ./phoenix;
+    };
   };
-  #   templates = {
-  #     example.path = ./example;
-  #     phoenix.path = ./phoenix;
-  #   };
-  # };
 }
